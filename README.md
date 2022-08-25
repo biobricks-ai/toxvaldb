a3b7fa1e8a0bffa155569a8f74ea8ced1a693a83
Thomas Luechtefeld <tom@insilica.co>
2022-08-25 11:20:55 -0400

    fixed renaming issue

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ dvc get -o toxvaldb https://github.com/biobricks-ai/toxvaldb data/
 
 Install with biobricks-r:
 ```R
-library(biobricks)
+library(biobricks) # devtools::install_github("biobricks-ai/biobricks-r")
 biobricks::brick_install("toxvaldb")
 biobricks::brick_pull("toxvaldb")
 toxvaldb <- biobricks::brick_load("toxvaldb")
--- stages/1_process.R
+++ stages/1_process.R
@@ -3,5 +3,5 @@ pacman::p_load(tidyverse)
 # only one csv right now
 csv <- fs::dir_ls("download",recurse=T, type="file", regexp=".csv$")
 tbl <- readr::read_csv(csv[1])
-tbl |> rename(year=year...48, publication.year=year...62)
+tbl <- tbl |> rename(year=year...48, publication.year=year...62)
 arrow::write_parquet(tbl,"data/toxvaldb.parquet") # parquet is less than 200mb
